c3f0e1231c45d6f35abb540c7d595d5e2fa5b01c
Michael Howard (CYBER SECURITY) <mikehow@ntdev.microsoft.com>
2024-05-02 14:01:12 -0500

    Changed hash to SHA512 to keep Steven happy

--- SimplisticSig/Program.cs
+++ SimplisticSig/Program.cs
@@ -5,7 +5,7 @@ string data = "We attack over the ridge at 1300Z";
 byte[] dataBytes = Encoding.UTF8.GetBytes(data);
 
 using var ecdsa = new ECDsaCng();
-byte[] ecdsaSignature = ecdsa.SignData(dataBytes, HashAlgorithmName.SHA256);
+byte[] ecdsaSignature = ecdsa.SignData(dataBytes, HashAlgorithmName.SHA512);
 Console.WriteLine($"ECDSA Sig: {Convert.ToBase64String(ecdsaSignature)}");
 bool ecdsaIsVerified = ecdsa.VerifyData(dataBytes, ecdsaSignature);
 Console.WriteLine($"ECDSA Signature Verified: {ecdsaIsVerified}");
@@ -15,6 +15,6 @@ Console.WriteLine();
 using var rsa = new RSACryptoServiceProvider();
 byte[] rsaSignature = rsa.SignData(dataBytes, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
 Console.WriteLine($"RSA Sig: {Convert.ToBase64String(rsaSignature)}");
-bool rsaIsVerified = rsa.VerifyData(dataBytes, rsaSignature, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+bool rsaIsVerified = rsa.VerifyData(dataBytes, rsaSignature, HashAlgorithmName.SHA512, RSASignaturePadding.Pkcs1);
 Console.WriteLine($"RSA Signature Verified: {rsaIsVerified}");
 
